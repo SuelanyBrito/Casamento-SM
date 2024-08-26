df754d0d70f930269c969829a9d081e3057eb6d4
Suelany Brito <Suelanybr@gmail.com>
2024-08-26 16:28:16 -0300

    add general list

--- src/app/pages/lista-casamento/lista-casamento.component.ts
+++ src/app/pages/lista-casamento/lista-casamento.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-lista-casamento',
   templateUrl: './lista-casamento.component.html',
-  styleUrls: ['./lista-casamento.component.css']
+  styleUrls: ['./lista-casamento.component.scss']
 })
 export class ListaCasamentoComponent {
 
