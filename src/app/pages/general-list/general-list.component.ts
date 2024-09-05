15a4c41a64b12dd7de1e4f24d557c64400a1fc0c
Suelany Brito <Suelanybr@gmail.com>
2024-09-05 13:25:34 -0300

    view list of the notion

--- src/app/pages/general-list/general-list.component.ts
+++ src/app/pages/general-list/general-list.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import data from '../../../assets/json/list.json';
 import { BackService } from 'src/app/services/backService';
+import { Observable } from "rxjs";
+
+interface Item {
+  id: string;
+  title: string;
+  number: number;
+  check: boolean;
+}
 
 @Component({
   selector: 'app-general-list',
@@ -8,27 +15,11 @@ import { BackService } from 'src/app/services/backService';
   styleUrls: ['./general-list.component.scss']
 })
 export class GeneralListComponent implements OnInit {
-  public list: any[] = [];
-  private intervalId: any;
+  public list$!: Observable<Item[]>;
 
   constructor(private backService: BackService) { }
 
   ngOnInit(): void {
-    this.backService.getList();
-    this.list = data.names;
-    this.fetchData();
+    this.list$ = this.backService.getList();
   }
-
-  async fetchData(): Promise<void> {
-    try {
-      const data = await this.backService.getList();
-      console.log('Data received:', data);
-    } catch (error) {
-      console.error('Error:', error);
-    } finally {
-      // Chama a função novamente após 20 segundos
-      this.intervalId = setTimeout(() => this.fetchData(), 20000);
-    }
-
-}
 }
--- src/app/services/backService.ts
+++ src/app/services/backService.ts
@@ -1,33 +1,51 @@
 import { Injectable } from "@angular/core";
 import { Apollo, gql } from "apollo-angular";
-import { map } from "rxjs"
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+interface Item {
+  id: string;
+  title: string;
+  number: number;
+  check: boolean;
+}
 
 @Injectable({
     providedIn: 'root'
 })
 export class BackService {
-    constructor(private apollo: Apollo){}
+  constructor(private apollo: Apollo) {}
 
-    getList(){
-        return this.apollo.watchQuery({
-            query: gql `query MyQuery {
-                            getList {
-                                results {
-                                id
-                                properties {
-                                    titleProperty {
-                                    title {
-                                        plain_text
-                                    }
-                                    }
-                                }
-                                }
-                            }
-}`,
-        }).valueChanges.pipe (
-            map((result: any)=>{
-                console.log("Graphql result", result)
-                return result.data.getList.results
-            })
-        )}
+  getList(): Observable<Item[]> {
+    return this.apollo.watchQuery({
+      query: gql`
+            query GetList {
+              getList {
+                results {
+                  id
+                  properties {
+                    titleProperty {
+                      title {
+                        plain_text
+                      }
+                    }
+                    marcado {
+                      checkbox
+                    }
+                    qtde {
+                      number
+                    }
+                  }
+                }
+              }
+          }`,
+    }).valueChanges.pipe(
+      map((result: any) => result.data.getList.results.map((item: any) => ({
+        id: item.id,
+        title: item.properties.titleProperty.title[0].plain_text,
+        number: item.properties.qtde.number,
+        check: item.properties.marcado.checkbox
+      })))
+    );
+  }
 }
